0a0209f8da47d713c9f8d108a0929bc267badda4
Yoanna Yotova <yyotova@asteasolutions.com>
2020-12-30 15:27:40 +0200

    Modify home page

--- scripts/landmarks.js
+++ scripts/landmarks.js
@@ -4,17 +4,25 @@
 
   xhr.onreadystatechange = function() {
     if(xhr.readyState == 4 && xhr.status == 200) {
-      var users = JSON.parse(xhr.responseText);
-      let names = "";
-      users.map((itm,idx) => {
-        names += `<div class="landmark"> 
+      var landmark = JSON.parse(xhr.responseText);
+      let info = "";
+      landmark.map((itm,idx) => {
+        info = "";
+        info += `<div class="landmark"> 
           <h3>${itm.name}</h3>
-          <h4>${itm.location}</h4>
+          <div>
+            <img class="location-icon" src="../location.png"  alt="location-icon">
+            <span>${itm.location}</span>
+          </div>
           <img src="${itm.img}">
           <p>${itm.description}</p>
+          <div class="add-favorite">
+            <i class="fa fa-heart"></i>
+            <p> Добави в любими </p>
+          </div>
           `;
+        document.getElementById('landmarks').innerHTML += info;
       }, this);
-      document.getElementById('landmarks').innerHTML = names;
     }
   }
 
